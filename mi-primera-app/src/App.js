4e8b7c6c1df1979c22b8d616ed52bc90a5caa9b3
kumakibear <128429317+kumakibear@users.noreply.github.com>
2024-01-15 13:32:28 +0000

    mensaje

--- mi-primera-app/src/App.js
+++ mi-primera-app/src/App.js
@@ -24,30 +24,19 @@ Resumen: Cuando implementamos flujos de datos es muy común olvidar algunos deta
         "
       
       />
-      <EventoC title="¡Ven a conocer a la comunidad!"
-      image="evento3"
-      dateLiteral="13 Noviembre de 2023 - 27 de Noviembre de 2023
-      La Paz - Bolivia"
-      placeLiteral="¡Hola Pythonista!
-
-      Participa en este Evento Presencial de la comunidad Python La Paz"
-      description="¿Sabías que existe una comunidad de Python en la ciudad de La Paz?
-
-      Entérate de todas las actividades y proyectos que realiza la comunidad Python La Paz
-      
-      ¡Descubre cómo participar!
-      
-      Completa el formulario de registro y participa del sorteo de asombrosos premios el día de la presentación
-      
-      Fechas
-      
-      Aula 105 - Carrera de Estadística - UMSA - Lunes 13 de Noviembre de 12:00 - 14:00
-      Campus Achocalla - Universidad Privada de Bolivia - Miércoles 15 de Noviembre de 12:00 - 14:00
-      Paraninfo - Universidad del Valle Sede La Paz - Martes 21 de Noviembre de 16:00 - 18:00
-      Sala Audiovisual - Carrera de Informática - UMSA - Jueves 23 de Noviembre de 16:00 - 18:00
-      Auditorio 4 - Universidad Católica Boliviana - Lunes 27 de Noviembre de 18:30 - 20:30" 
-      
-      />
+      <EventoC title="MeetUp 11 - 3 Pitfalls de la Ingeniería de datos (donde no meter la pata)"
+        image="evento2"
+        dateLiteral="25 de Noviembre de 2023"
+        placeLiteral="Online"
+        description={[
+          "¡Hola Pythonista!",
+          "Acompáñanos al MeetUp mensual de la comunidad Python La Paz",
+          "En este MeetUp tendremos la participación de Valery Calderon Briz, Ingeniera en Telecomunicaciones actualmente trabajando como Ingeniera de datos Senior en One.Five",
+          "Anteriormente Co-organizadora de Pyladies MX y Líder de la comunidad Python Guatemala",
+          "ha impartido conferencias y talleres en diversas PyCon en múltiples países.",
+        ]}
+      /> 
+     
       
     </div>
   );
--- mi-primera-app/src/components/EventoC.js
+++ mi-primera-app/src/components/EventoC.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "../styles/Evento.css";
-function Evento(props) {
-  return (
-    <div className="card-container">
+function EventoC({ image, title, dateLiteral, placeLiteral, description }) {
+    return (
+        <div className="card-container">
 
-      <img className="card-image"
-        src={require(`../images/${props.image}.jpeg`)}
-        alt="Foto del evento" />
+            <img className="card-image"
+                src={require(`../images/${image}.jpeg`)}
+                alt="Foto del evento" />
 
 
-      <div className="card-content">
-        <h2 className="card-title">{props.title}</h2>
-        <p className="card-date">{props.dateLiteral}</p>
-        <p className="card-place">{props.placeLiteral}</p>
-        <p className="card-description">{props.description}</p>
-      </div>
-    </div>
-  );
+            <div className="card-content">
+                <h2 className="card-title">{title}</h2>
+                <p className="card-date">{dateLiteral}</p>
+                <p className="card-place">{placeLiteral}</p>
+                {description.map((line, index) => (<p className="card-description">{line}</p>))}
+            </div>
+        </div>
+    );
 }
 
-export default Evento;
+export default EventoC;
